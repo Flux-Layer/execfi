192f3468e166bb777fd7467daa40cc1829a8f199
TopengDev <topengdev@outlook.com>
2025-09-13 17:43:10 +0700

    :hammer: ~ terminal display

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import useZeroDevSA from "../hooks/useZeroDevSA";
 import PageBarLoader from "../components/loader";
 import PromptTextInput from "../components/text-input/prompt-input";
 import { usePrivy, } from "@privy-io/react-auth";
+import TerminalContact from "../components/terminal-input"
 
 export default function Home() {
   const privyInstance = usePrivy();
@@ -11,7 +12,7 @@ export default function Home() {
   
   return (
     <main className="w-full h-screen flex items-center justify-center bg-black">
-      {privyInstance?.ready ? <PromptTextInput /> : <PageBarLoader />}
+      {privyInstance?.ready ? <TerminalContact /> : <PageBarLoader />}
     </main>
   );
 }
